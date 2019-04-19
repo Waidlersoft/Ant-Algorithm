d65d028322f95078452d1427826abef3872124c6
Waidlersoft <49609519+Waidlersoft@users.noreply.github.com>
2019-04-19 18:35:31 +0200

    Add files via upload
    drawing of ants dynamically and consoleoutput of status

--- ant.py
+++ ant.py
@@ -8,8 +8,12 @@ target = ["090090"]
 n = 1 #number of ants
 x_border = [0,100]
 y_border = [0,100]
-            
+statuslist = ["search","return"]
+
 def main():
+    ax = plt.axes()
+    plt.xlim(0,100) 
+    plt.ylim(0,100)
     playgr = field.start()
     ants = ant.create()
     pos_list =[]
@@ -17,15 +21,12 @@ def main():
         for i in range(0,n):
             smell_list = ant.smell(ants[i]["pos"],playgr)
             direction = ant.move(smell_list,ants[i]["status"])
-            ant.pheromons(ants[i]["pos"],playgr,ants[i]["status"])
+            ant.pheromons(ants[i]["pos"],playgr,common.switchstatus(ants[i]["status"]))
             antchange = ant.modechange(ants[i]["pos"],ants[i])
             ants.update(antchange)
             position = ant.position(ants,direction)
             pos_new = ants[i]["pos"]
-            pos_list.append(pos_new)
-    field.paint(pos_list)
-    plt.show()
-
+            field.paint(pos_new,ax)
   
 class common():
     def poskey(x,y):
@@ -36,6 +37,12 @@ class common():
         pos_x = int(pos[:3])
         pos_y = int(pos[4:6])
         return [pos_x,pos_y]
+
+    def switchstatus(status):
+        if status == "search":
+            i = "return"
+        else: i = "return"
+        return i
         
 class ant():
     def create(ants={},n=1):
@@ -47,11 +54,18 @@ class ant():
                         )
         return ants
 
-    def move(value_list=[1,1,1,1,1,1,1,1],status="border"):
+    def move(value_list=[1,1,1,1,1,1,1,1],status="search"):
             rnd = random.random()
             j = 0
             grade = 0
             val_sum = 0
+            text =str(status)+"-- search:"
+            for i in value_list:
+                text=text+str(i["search"])+","
+            text = text + "-- return:"
+            for i in value_list:
+                text=text+str(i["return"])+","
+            print(text)
             for i in value_list:
                 val_sum = val_sum + int(i[status])
             for i in value_list:
@@ -96,7 +110,6 @@ class ant():
         stat = a["status"]
         if pos == target:
             stat = "return"
-            
         elif pos == start:
             stat = "search"
         else:
@@ -107,6 +120,7 @@ class ant():
 
 class field():
     def start():
+        
         field = {}
         for x in range(0,101):
             for y in range(0,101):
@@ -115,21 +129,19 @@ class field():
                     n = 0
                 else:
                     n = 1
-                field.update({keystring:{"border":n,
-                                         "search":n,
+                field.update({keystring:{"search":n,
                                          "return":n
                                          }
                               }
                              )
         return field
 
-    def paint(pos):
-        x_list = []
-        y_list = []
-        for i in pos:
-            pos_value = common.xy_pos(i)
-            x_list.append(pos_value[0])
-            y_list.append(pos_value[1])
-        fig, ax = plt.subplots()
-        ax.plot(x_list, y_list,'o')
+    def paint(pos,ax):       
+            x= common.xy_pos(pos)[0]
+            y= common.xy_pos(pos)[1]
+            ax.plot([x], [y], 'ro')
+            plt.draw() 
+            plt.pause(0.01)
+            ax.lines[0].remove()
+
 main()
