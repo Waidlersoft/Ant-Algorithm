cb8dba4c7f7317f68c2ecb4af6f8985ece72c016
Waidlersoft <49609519+Waidlersoft@users.noreply.github.com>
2019-04-22 10:37:32 +0200

    Update log.py
    Log-functions updated

--- log.py
+++ log.py
@@ -1,8 +1,16 @@
 import datetime
 class log():
+    Level = ["Info","Log","Debug"]
+    def create():
+        with ('log.txt','w') Open as f:
+           f.write('Ant-algorithm log')
+
     def write(f,text,status):
-        pot = datetime.datetime.now().isoformat()[:19]
-        output= pot+" "+status+":"+str(text)
-        if status == "info":
-            print(output)
-            f.write(output)
+        with ('log.txt','a') Open as f:
+         pot = datetime.datetime.now().isoformat()[:19]
+         output= pot+" "+status+":"+str(text)
+         for i in Level:
+             print(output)
+             f.write(output)
+             If i==Status:
+               break
