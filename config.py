9519500aa465dbccece8a5ff493a5e89cb2fd729
wolfp <peter.wolf1981@web.de>
2020-03-18 17:36:28 +0100

    Update

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 direction = [[1, -1], [1, 0], [1, 1], [0, 1], [-1, 1], [-1, 0], [-1, -1], [0, -1]]
 startpoint = "001001"
 targetpoint = "009009"
-max_ants = 50
+max_ants = 5
 n = max_ants
 pos_list = []
 x_border = [0, 11]
